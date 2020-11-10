f4f197726fa06ed848a68c2b89426765ccc66db1
javimatz <jvdelso@gmail.com>
2020-11-10 17:55:33 +0100

    start files

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator()
+
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigation.navigate('Profile')
+        }
+      />
       <StatusBar style="auto" />
     </View>
+  )
+}
+
+const ProfileScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Go to Home Screen"
+        onPress={() =>
+          navigation.navigate('Home')
+        }
+      />
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name='Home' 
+          component={HomeScreen} 
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen 
+          name='Profile' 
+          component={ProfileScreen} 
+          options={{ title: 'Profile Screen' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -19,3 +64,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
