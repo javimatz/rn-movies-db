4cfeef11e269cdb3fd652160b6f470cc3c1c2943
javimatz <jvdelso@gmail.com>
2020-11-11 17:37:53 +0100

    add: MovieScreen component

---
+++ src/screen/MovieScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const MovieScreen = () => {
+  return (
+  	<Text>Movie Screen</Text>	
+  );
+}
+
+export default MovieScreen
