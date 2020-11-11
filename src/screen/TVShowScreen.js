cfb1c44c95e7788121631a31777fad4f1aca6834
javimatz <jvdelso@gmail.com>
2020-11-11 17:38:12 +0100

    add: TVShowScreen component

---
+++ src/screen/TVShowScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const TVShowScreen = () => {
+  return (
+  	<Text>TV Show Screen</Text>	
+  );
+}
+
+export default TVShowScreen
