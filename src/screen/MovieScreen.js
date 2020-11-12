a5f253bac76ee101247537d04a87b436aa09d11a
javimatz <jvdelso@gmail.com>
2020-11-12 14:04:42 +0100

    add: MovieScreen component

--- src/screen/MovieScreen.js
+++ src/screen/MovieScreen.js
@@ -1,10 +1,42 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import PropTypes from 'prop-types';
 
-const MovieScreen = () => {
-  return (
-  	<Text>Movie Screen</Text>	
-  );
+import HomeComponent from '../components/Home/HomeComponent' 
+import { requestMovieScreen as RequestMovieApi } from '../api/api' 
+
+import { MovieTypes } from "../helper/Types";
+
+class MovieScreen extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			moviesData: []
+		} 
+	}
+
+	componentDidMount() {
+    	this.requestMovieScreen();
+  	}
+
+	requestMovieScreen = async () => {
+		await RequestMovieApi((data) => this.setState({ moviesData: data }));
+	};
+
+	render () {
+		return (
+			<HomeComponent
+		        type={"movie"}
+		        subTitle={MovieTypes}
+		        navigation={this.props.navigation}
+		        data={this.state.moviesData}
+		        onRefresh={this.requestMovieScreen}
+		    />
+		);
+	}
+}
+
+MovieScreen.propTypes = {
+	navigation: PropTypes.object
 }
 
 export default MovieScreen
